417b9b45987d0e5cb17ce96e649690b1d0f2e6c7
viktor-lima <joaolimak10@outlook.com>
2022-01-25 22:44:21 -0300

    criando home da loja pt1

---
+++ js/efeitos.js
@@ -0,0 +1,54 @@
+$(document).ready(function(){
+
+	$("#logotipo").on("mouseover",function(){
+		
+		$("#banner h1").addClass("efeito");
+
+	}).on("mouseout", function(){
+
+		$("#banner h1").removeClass("efeito");
+
+	});
+
+	$("#input-search").on("focus", function(){
+
+		$("li.search").addClass("ativo");
+
+	}).on("blur", function(){
+
+		$("li.search").removeClass("ativo");				
+
+	});
+
+	$(".thumbnails").owlCarousel({
+ 
+      //autoPlay: 3000,
+      items : 4
+ 
+  	});
+
+  	var owl = $(".thumbnails").data('owlCarousel');
+
+  	$('#btn-news-prev').on("click", function(){
+
+  		owl.prev();
+
+  	});
+
+  	$('#btn-news-next').on("click", function(){
+
+  		owl.next();
+
+  	});
+
+  	$("#page-up").on("click", function(event){
+
+  		$("body").animate({
+  			scrollTop:0
+  		}, 1000);
+
+  		event.preventDefault();
+
+  	});
+
+});
---
+++ view/shop.php
@@ -0,0 +1,98 @@
+<?php
+include_once("header.php");
+?>
+
+<section>
+
+	<div class="container" id="destaque-produtos-container">
+		
+		<div id="destaque-produtos">
+
+			<div class="item">
+
+				<div class="col-sm-6 col-imagem">
+					<img src="img/produtos/moto-x.png" alt="Nome do produto">
+				</div>
+				<div class="col-sm-6 col-descricao">
+					<h2>
+						Smartphone Motorola X Play Dual chip Desbloqueado Android 5.1
+					</h2>
+					<div class="box-valor">
+						<div class="text-noboleto .text-arial-cinza ">no boleto</div>
+						<div class="text-por .text-arial-cinza ">por</div>
+						<div class="text-reais text-roxo">R$</div>
+						<div class="text-valor text-roxo">1.259</div>
+						<div class="text-valor-centavos text-roxo">,10</div>
+						<div class="text-parcelas .text-arial-cinza ">ou em até 8x de R$ 174,88</div>
+						<div class="text-total .text-arial-cinza ">total a prazo R$ 1.399,00</div>
+
+						
+					</div>
+					<a href="#" class="btn btn-comprar text-roxo"><i class="fas fa-cart-plus"></i> compre agora</a>
+				</div>
+			
+			</div>
+
+			<div class="item">
+
+				<div class="col-sm-6 col-imagem">
+					<img src="img/produtos/moto-x.png" alt="Nome do produto">
+				</div>
+				<div class="col-sm-6 col-descricao">
+					<h2>
+						Smartphone Motorola X Play Dual chip Desbloqueado Android 5.1
+					</h2>
+					<div class="box-valor">
+						<div class="text-noboleto .text-arial-cinza ">no boleto</div>
+						<div class="text-por .text-arial-cinza ">por</div>
+						<div class="text-reais text-roxo">R$</div>
+						<div class="text-valor text-roxo">1.259</div>
+						<div class="text-valor-centavos text-roxo">,10</div>
+						<div class="text-parcelas .text-arial-cinza ">ou em até 8x de R$ 174,88</div>
+						<div class="text-total .text-arial-cinza ">total a prazo R$ 1.399,00</div>
+
+						
+					</div>
+					<a href="#" class="btn btn-comprar text-roxo"><i class="fas fa-cart-plus"></i> compre agora</a>
+				</div>
+			
+			</div>
+
+		</div>
+		<button type="button" id="btn-destaque-prev"><i class="fa fa-angle-left"></i></button>
+		<button type="button" id="btn-destaque-next"><i class="fa fa-angle-right"></i></button>
+
+	</div>
+
+
+</section>
+
+<script src="https://kit.fontawesome.com/9ffce81729.js" crossorigin="anonymous"></script>
+
+<?php
+include_once("footer.php");
+?>
+
+<script>
+	$(function(){
+		$("#destaque-produtos").owlCarousel({
+			autoPlay:5000,
+			items : 1,
+			singleItem: true
+		});
+	});
+
+	var owlDestaque = $("#destaque-produtos").data('owlCarousel');
+
+  	$('#btn-destaque-prev').on("click", function(){
+
+  		owlDestaque.prev();
+
+  	});
+
+  	$('#btn-destaque-next').on("click", function(){
+
+  		owlDestaque.next();
+
+  	});
+</script>
